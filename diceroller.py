a4b31cf8e056bb2406dffb779aea1ab91e17c2dc
Riccardo Vandra <riccardo.vandra@gmail.com>
2019-06-16 14:00:11 +0200

    mean introduction

--- diceroller.py
+++ diceroller.py
@@ -7,11 +7,15 @@ start = input('Do you want to roll the dice ? Type Yes to start ')
 
 
 def stats():
+    print('Here are the stats !')
     # roll count
     # mean
+    sum_stats = sum(statistics)
+    len_stats = len(statistics)
+    mean = sum_stats/len_stats
+    print('mean: ' + str(mean))
     # most common number
     # least common number
-    print('Here are the stats')
 
 
 def roll():
@@ -19,11 +23,14 @@ def roll():
     number = random.randrange(1, 6)
     print('You got ' + str(number))
     statistics.append(number)
-    print(statistics)
     reroll = input('Would you like to reroll ? ')
     while reroll == 'yes' or reroll == 'y':
         roll()
     else:
+        stats_ask = input(
+            'Type stats to see all your result or type something else to end the game ')
+        if stats_ask == 'stats':
+            stats()
         print('Thank you for playing !')
         exit()
 
