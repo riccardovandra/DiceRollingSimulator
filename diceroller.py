50a4e9c222dbe5ed2e303b3262b70d171035618d
Riccardo Vandra <riccardo.vandra@gmail.com>
2019-06-16 15:38:25 +0200

    Stats completed

--- diceroller.py
+++ diceroller.py
@@ -3,11 +3,12 @@ import random
 statistics = list()
 rollcount = 0
 
-print('Welcome to Dice Rolling Simulator v1.5')
+print('Welcome to Dice Rolling Simulator v2')
 start = input('Do you want to roll the dice ? Type Yes to start ')
 
 
 def stats():
+    most_common = list()
     print('Here are the stats !')
     # roll count
     print('Roll Count:', rollcount)
@@ -15,13 +16,29 @@ def stats():
     sum_stats = sum(statistics)
     len_stats = len(statistics)
     mean = sum_stats/len_stats
-    print('mean: ' + str(mean))
-    # most common number
-    # least common number
-    # counter stats
+    print('mean:', mean)
+    # get count
     for x in [1, 2, 3, 4, 5, 6]:
         count = statistics.count(x)
-        print(x, 'rolled', count, 'times')
+        most_common.append([x, count])
+    # sort based on the number of times a number is rolled
+
+    def sortSecond(val):
+        return val[1]
+    mc_sorted = sorted(most_common, key=sortSecond, reverse=True)
+    # most common number
+    print('most common number:', mc_sorted[0]
+          [0], 'rolled', mc_sorted[0][1], 'times')
+    # least common number
+    print('least common number:',
+          mc_sorted[-1][0], 'rolled', mc_sorted[-1][1], 'times')
+
+    # counter stats
+    print('Additional Stats')
+    print('')
+    for key, value in most_common:
+        print(key, 'rolled', value, 'times')
+    print(most_common)
 
 
 def roll():
--- readme.md
+++ readme.md
@@ -3,4 +3,9 @@ Objective: Create a simple Dice Rolling Simulator
 1. Roll a dice and get a number
 2. Print the number
 3. Reroll Possibility
-4. Stats Introduction
+4. Stats
+   - Roll Count
+   - Mean
+   - Most Common Number
+   - Least Common Number
+   - Stats around each rolled number
