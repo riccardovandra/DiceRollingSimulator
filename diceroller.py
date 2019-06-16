f2eda363378d0b1e249e764dd0677d802e314baa
Riccardo Vandra <riccardo.vandra@gmail.com>
2019-06-16 13:34:35 +0200

    Reroll Possibility

--- diceroller.py
+++ diceroller.py
@@ -1,15 +1,20 @@
 import random
 
+print('Welcome to Dice Rolling Simulator 1.0')
 start = input('Do you want to roll the dice ? Type Yes to start ')
 
 
 def roll():
+    print('You started rolling the dice')
     number = random.randrange(1, 6)
     print('You got ' + str(number))
+    reroll = input('Would you like to reroll ? ')
+    while reroll == 'yes':
+        roll()
+    else:
+        print('Thank you for playing !')
+        exit()
 
 
 if start == 'yes':
-    print('You started rolling the dice')
     roll()
-else:
-    exit()
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
-Objective:
+Objective: Create a simple Dice Rolling Simulator
 
 1. Roll a dice and get a number
 2. Print the number
+3. Reroll Possibility
